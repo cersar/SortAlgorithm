a9be5e284d1bbe2c8759075ad06d894279dcf159
cersar <yangsen1991@outlook.com>
2018-09-11 21:25:32 +0800

    增加flag用来判断[start~end-i]是否有序，如果有序，则整个序列有序，终止排序

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,40 +1,48 @@
-/*
- * bubble_sort.c
- *
- * 经典冒泡实现
- *  Created on: Sep 6, 2016
- *      Author: yangsen
- */
+#include<stdio.h>
+#include<time.h>
 
-#include <stdio.h>
-#define SIZE 8
 
-void bubble_sort(int a[], int n);
-
-void bubble_sort(int a[], int n)
-{
-    int i, j, temp;
-    for (j = 0; j < n - 1; j++)
-        for (i = 0; i < n - 1 - j; i++)
-        {
-            if(a[i] > a[i + 1])
-            {
-                temp = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = temp;
-            }
-        }
+void swap(int *i, int *j) {
+	int tmp = *i;
+	*i = *j;
+	*j = tmp;
 }
 
-int main()
-{
-    int number[SIZE] = {95, 45, 15, 78, 84, 51, 24, 12};
-    int i;
-    bubble_sort(number, SIZE);
-    for (i = 0; i < SIZE; i++)
-    {
-        printf("%d", number[i]);
-    }
-    printf("\n");
+void bubbleSort(int a[],int start,int end) {
+	clock_t startTime, endTime;
+	startTime = clock();
+	double  duration;
+	int flag;
+	long cnt = 0;
+	for (int i = start; i <= end-1; ++i) {
+		flag = false;
+		for (int j = start; j <= end -1 - i; j++) {
+			cnt++;
+			if (a[j] > a[j + 1]) {
+				swap(&a[j], &a[j + 1]);
+				flag = true;
+			}
+		}
+		if (flag == false) break;
+		
+	}
+	endTime = clock();
+	duration = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+	printf("%f seconds\n", duration);
+	printf("Total detection times: %d\n", cnt);
+
 }
 
+void printArray(int a[], int start, int end) {
+	for (int i = start; i <= end; ++i) {
+		printf("%d\t", a[i]);
+	}
+	printf("\n");
+}
+
+int main() {
+	int a[] = { 1,9,2,8,3,7,4,6 };
+	bubbleSort(a, 0, sizeof(a) / sizeof(int) - 1);
+	printArray(a, 0, sizeof(a) / sizeof(int) - 1);
+	return 0;
+}
