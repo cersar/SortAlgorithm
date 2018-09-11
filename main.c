a2166810699907561e0396b354c28da30ec1e727
cersar <yangsen1991@outlook.com>
2018-09-11 22:03:50 +0800

    增加selectSort

--- main.c
+++ main.c
@@ -8,28 +8,55 @@ void swap(int *i, int *j) {
 	*j = tmp;
 }
 
-void bubbleSort(int a[],int start,int end) {
+void bubbleSort(int a[], int start, int end) {
 	clock_t startTime, endTime;
 	startTime = clock();
 	double  duration;
 	int flag;
-	long cnt = 0;
-	for (int i = start; i <= end-1; ++i) {
+	long cntDect = 0, cntSwap = 0;
+	for (int i = start; i <= end - 1; ++i) {
 		flag = false;
-		for (int j = start; j <= end -1 - i; j++) {
-			cnt++;
+		for (int j = start; j <= end - 1 - i; j++) {
+			cntDect++;
 			if (a[j] > a[j + 1]) {
 				swap(&a[j], &a[j + 1]);
+				cntSwap++;
 				flag = true;
 			}
 		}
 		if (flag == false) break;
+
+	}
+	endTime = clock();
+	duration = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+	printf("%f seconds\n", duration);
+	printf("Total detection times: %d\n", cntDect);
+	printf("Total swap times: %d\n", cntSwap);
+}
+
+void selectSort(int a[],int start,int end) {
+	clock_t startTime, endTime;
+	startTime = clock();
+	double  duration;
+	int min;
+	long cntDect = 0, cntSwap = 0;
+	for (int i = start; i <= end-1; ++i) {
+		min = i;
+		for (int j = i+1; j <= end; j++) {
+			cntDect++;
+			if (a[min] > a[j]) {
+				min = j;
+			}
+		}
+		swap(&a[i], &a[min]);
+		cntSwap++;
 		
 	}
 	endTime = clock();
 	duration = (double)(endTime - startTime) / CLOCKS_PER_SEC;
 	printf("%f seconds\n", duration);
-	printf("Total detection times: %d\n", cnt);
+	printf("Total detection times: %d\n", cntDect);
+	printf("Total swap times: %d\n", cntSwap);
 
 }
 
@@ -42,7 +69,8 @@ void printArray(int a[], int start, int end) {
 
 int main() {
 	int a[] = { 1,9,2,8,3,7,4,6 };
-	bubbleSort(a, 0, sizeof(a) / sizeof(int) - 1);
+//	bubbleSort(a, 0, sizeof(a) / sizeof(int) - 1);
+//	SelectSort(a, 0, sizeof(a) / sizeof(int) - 1);
 	printArray(a, 0, sizeof(a) / sizeof(int) - 1);
 	return 0;
 }
