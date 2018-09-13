58bf2654da3756caf7e97fa7e2d2b58978a4437e
cersar <yangsen1991@outlook.com>
2018-09-13 16:21:13 +0800

    增加快速排序

--- main.c
+++ main.c
@@ -73,6 +73,30 @@ void insertSort(int a[], int start, int end, long &cntDect, long &cntSwap) {
 	cntSwap /= 2;
 }
 
+
+//shell排序
+void shellSort(int a[], int start, int end, long &cntDect, long &cntSwap) {
+	int key;
+	int i, j,step=1;
+	
+	while(step<=end-start) step=step*3+1;
+	step=(step-1)/3;
+	while(step>=1){
+		for (i = start+step; i <= end; i++) {
+			key = a[i];
+			for (j = i - step; j >= start&&a[j] > key; j-=step) {
+				cntDect++;
+				a[j + step]=a[j];
+				cntSwap++;
+			}
+			a[j+step]=key;
+			cntSwap++;
+		}
+		step=(step-1)/3;
+	}
+	
+}
+
 void merge(int a[], int b[], int start, int end, int mid, long &cntDect, long &cntSwap) {
 	for (int i = start; i <= mid; ++i) {
 		cntDect++;
@@ -102,29 +126,6 @@ void merge(int a[], int b[], int start, int end, int mid, long &cntDect, long &c
 	}
 }
 
-//shell排序
-void shellSort(int a[], int start, int end, long &cntDect, long &cntSwap) {
-	int key;
-	int i, j,step=1;
-	
-	while(step<=end-start) step=step*3+1;
-	step=(step-1)/3;
-	while(step>=1){
-		for (i = start+step; i <= end; i++) {
-			key = a[i];
-			for (j = i - step; j >= start&&a[j] > key; j-=step) {
-				cntDect++;
-				a[j + step]=a[j];
-				cntSwap++;
-			}
-			a[j+step]=key;
-			cntSwap++;
-		}
-		step=(step-1)/3;
-	}
-	
-}
-
 //归并排序
 void mergeSort(int a[], int b[], int start, int end, long &cntDect, long &cntSwap) {
 	if (start < end) {
@@ -135,6 +136,41 @@ void mergeSort(int a[], int b[], int start, int end, long &cntDect, long &cntSwa
 	}
 
 }
+
+int partation(int a[],int start,int end, long &cntDect, long &cntSwap){
+	int p = start-1,q=end+1,key=a[start];
+	while(1){
+		do{
+			p++;
+		}while(a[p]<key);
+		do{
+			q--;
+		}while(a[q]>key);
+		if(p<q){
+			swap(&a[p],&a[q]);
+		}else{
+			//这里选q作为分割点，原因是结束时q∈[start,end),保证q+1和q不超出[start,end],防止死循环 
+			//证明q∈[start,end):
+			// 1)证明q>=start：注意到，每执行一次swap，意味着将小于等于key的值置于q之前，执行swap的次数>=0,
+			//当循环结束时执行swap次数>0时，则循环结束之前，q的前面至少有一个小于key的值，循环结束时必有q>=start;
+			//当循环结束时执行swap的次数为0时，说明外层while循环只执行一次，此时p=start=q,q>=start也成立
+			//综上，q>=start 
+			//2)证明q<end：(反证法)首先，显然q<=end,假设q=end，即外层while循环执行一次便跳出
+			//此时p=start&&p>=q,则q=start=end,这与start<end矛盾，故q<end; 
+			return q;
+		}
+	}
+}
+
+//快速排序
+void quickSort(int a[], int start, int end, long &cntDect, long &cntSwap) {
+	if(start<end){
+		int p = partation(a,start,end,cntDect,cntSwap);
+		quickSort(a,start,p,cntDect,cntSwap);
+		quickSort(a,p+1,end,cntDect,cntSwap);		
+	}	
+}
+
 
 int main() {
 	clock_t startTime, endTime;
